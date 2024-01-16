d13f749dbc47e199c4df579bec8bfd16b751df84
PedroVidalDev <pedrohvidals@gmail.com>
2024-01-16 02:50:21 -0300

    [feat] - creating dto for task output and add a mapping method

--- src/main/java/com/pedro/simplify/application/dto/TaskOutputDTO.java
+++ src/main/java/com/pedro/simplify/application/dto/TaskOutputDTO.java
@@ -1,4 +1,9 @@
 package com.pedro.simplify.application.dto;
 
-public record TaskOutputDTO() {
+import com.pedro.simplify.infrastructure.entities.Task;
+
+public record TaskOutputDTO(Long id, String name, String description, boolean realized, int priority) {
+    public TaskOutputDTO(Task data){
+        this(data.getId(), data.getName(), data.getDescription(), data.isRealized(), data.getPriority());
+    }
 }
