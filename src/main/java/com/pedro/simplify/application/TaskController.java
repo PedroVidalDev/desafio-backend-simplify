4f423936b898db744819fd6a540d8aef28d3e5d5
PedroVidalDev <pedrohvidals@gmail.com>
2024-01-16 02:50:34 -0300

    [feat] - creating the get one, get all and delete methods in controller

--- src/main/java/com/pedro/simplify/application/TaskController.java
+++ src/main/java/com/pedro/simplify/application/TaskController.java
@@ -1,18 +1,20 @@
 package com.pedro.simplify.application;
 
 import com.pedro.simplify.application.dto.TaskInputDTO;
+import com.pedro.simplify.application.dto.TaskOutputDTO;
 import com.pedro.simplify.infrastructure.entities.Task;
 import com.pedro.simplify.infrastructure.repositories.TaskRepository;
 import jakarta.transaction.Transactional;
+import lombok.Getter;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/task")
+@RequestMapping("/tasks")
 public class TaskController {
 
     @Autowired
@@ -24,5 +26,27 @@ public class TaskController {
         repository.save(new Task(data));
 
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity getAllTask(){
+        var listTasks = repository.findAll().stream().map(TaskOutputDTO::new).toList();
+
+        return ResponseEntity.ok(listTasks);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOneTask(@PathVariable String id){
+        var task = repository.getReferenceById(Long.parseLong(id));
+
+        return ResponseEntity.ok(new TaskOutputDTO(task));
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteTask(@PathVariable String id){
+        repository.deleteById(Long.parseLong(id));
+
+        return ResponseEntity.noContent().build();
     }
 }
