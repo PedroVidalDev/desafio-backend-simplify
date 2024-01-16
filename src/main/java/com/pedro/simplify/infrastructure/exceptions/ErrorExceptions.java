daab5f4e11bffc4fc55bd97831119bb37d83ebd4
PedroVidalDev <pedrohvidals@gmail.com>
2024-01-16 06:19:04 -0300

    [feat] - adding error handling to the application

---
+++ src/main/java/com/pedro/simplify/infrastructure/exceptions/ErrorExceptions.java
@@ -0,0 +1,27 @@
+package com.pedro.simplify.infrastructure.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class ErrorExceptions {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity entityNotFound(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity noSuchElement(){
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(TaskValidation.class)
+    public ResponseEntity taskValidation(TaskValidation e){
+        return ResponseEntity.badRequest().body(e.getMessage());
+    }
+}
