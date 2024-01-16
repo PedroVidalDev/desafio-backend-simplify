eadd8b498aba5a7313e00a477c177f97870c346f
PedroVidalDev <pedrohvidals@gmail.com>
2024-01-16 06:20:07 -0300

    [feat] - creating function in task service to valid the actions

--- src/main/java/com/pedro/simplify/application/TaskController.java
+++ src/main/java/com/pedro/simplify/application/TaskController.java
@@ -3,6 +3,7 @@ package com.pedro.simplify.application;
 import com.pedro.simplify.application.dto.TaskInputDTO;
 import com.pedro.simplify.application.dto.TaskInputUpdateDTO;
 import com.pedro.simplify.application.dto.TaskOutputDTO;
+import com.pedro.simplify.domain.TaskService;
 import com.pedro.simplify.infrastructure.entities.Task;
 import com.pedro.simplify.infrastructure.repositories.TaskRepository;
 import jakarta.transaction.Transactional;
@@ -23,12 +24,14 @@ public class TaskController {
     @Autowired
     private TaskRepository repository;
 
+    @Autowired
+    private TaskService service;
+
     @PostMapping
     @Transactional
     public ResponseEntity createTask(@RequestBody TaskInputDTO data, UriComponentsBuilder uriBuild){
-        Task task = new Task(data);
 
-        repository.save(task);
+        var task = service.createNewTaskVerify(data);
 
         var uri = uriBuild.path("/tasks/{id}").buildAndExpand(task.getId()).toUri();
 
@@ -52,7 +55,7 @@ public class TaskController {
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity deleteTask(@PathVariable String id){
-        repository.deleteById(Long.parseLong(id));
+        service.deleteTaskVerify(id);
 
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/pedro/simplify/domain/TaskService.java
+++ src/main/java/com/pedro/simplify/domain/TaskService.java
@@ -1,4 +1,35 @@
 package com.pedro.simplify.domain;
 
+import com.pedro.simplify.application.dto.TaskInputDTO;
+import com.pedro.simplify.infrastructure.entities.Task;
+import com.pedro.simplify.infrastructure.exceptions.TaskValidation;
+import com.pedro.simplify.infrastructure.repositories.TaskRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
+    @Autowired
+    private TaskRepository repository;
+
+    public Task createNewTaskVerify(TaskInputDTO dto){
+        if(repository.existsByName(dto.name())){
+            throw new TaskValidation("That task already exists.");
+        }
+        else{
+            Task task = new Task(dto);
+            repository.save(task);
+            return task;
+        }
+    }
+
+    public void deleteTaskVerify(String id) {
+        if(!repository.existsById(Long.parseLong(id))){
+            throw new EntityNotFoundException();
+        }
+        else{
+            repository.deleteById(Long.parseLong(id));
+        }
+    }
 }
