1884f460cd244c3aec0e0f73ed0f98985d67d841
PedroVidalDev <pedrohvidals@gmail.com>
2024-01-16 02:30:06 -0300

    [feat] - creating the controller and the post method of task

--- src/main/java/com/pedro/simplify/application/TaskController.java
+++ src/main/java/com/pedro/simplify/application/TaskController.java
@@ -1,7 +1,10 @@
 package com.pedro.simplify.application;
 
 import com.pedro.simplify.application.dto.TaskInputDTO;
+import com.pedro.simplify.infrastructure.entities.Task;
+import com.pedro.simplify.infrastructure.repositories.TaskRepository;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +14,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/task")
 public class TaskController {
+
+    @Autowired
+    private TaskRepository repository;
+
     @PostMapping
     @Transactional
     public ResponseEntity createTask(@RequestBody TaskInputDTO data){
+        repository.save(new Task(data));
 
+        return ResponseEntity.ok().build();
     }
 }
