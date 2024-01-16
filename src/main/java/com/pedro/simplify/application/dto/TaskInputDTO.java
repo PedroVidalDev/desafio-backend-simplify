c562954fb8c922fb444aaaf21bb240199c2c3cdc
PedroVidalDev <pedrohvidals@gmail.com>
2024-01-16 02:49:54 -0300

    [refactor] - altering the data types in dto

--- src/main/java/com/pedro/simplify/application/dto/TaskInputDTO.java
+++ src/main/java/com/pedro/simplify/application/dto/TaskInputDTO.java
@@ -1,4 +1,4 @@
 package com.pedro.simplify.application.dto;
 
-public record TaskInputDTO(String name, String description, String realized, String priority) {
+public record TaskInputDTO(String name, String description, boolean realized, int priority) {
 }
