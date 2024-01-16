9a9549bb02c7d4255cd05c7108786036eb3ec1fa
PedroVidalDev <pedrohvidals@gmail.com>
2024-01-16 06:20:19 -0300

    [feat] - creating function exists by name in task repository

--- src/main/java/com/pedro/simplify/infrastructure/repositories/TaskRepository.java
+++ src/main/java/com/pedro/simplify/infrastructure/repositories/TaskRepository.java
@@ -4,4 +4,5 @@ import com.pedro.simplify.infrastructure.entities.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TaskRepository extends JpaRepository<Task, Long> {
+    boolean existsByName(String name);
 }
